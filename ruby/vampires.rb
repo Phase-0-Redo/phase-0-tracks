3e08dda956f2942ed45017ff6c5328422a2104d4
jtmunoz <jordantmunoz@gmail.com>
2016-08-01 14:36:19 -0700

    adds until loops for question garlic, question insurance

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -7,11 +7,38 @@ age = gets.chomp
 puts "What year were you born?"
 year = gets.chomp
 
-puts "Our company cafeteria serves garlic bread. Should we order some for you?"
-garlic = gets.chomp
+valid_input = false
+  until valid_input
+  puts "Our company cafeteria serves garlic bread. Should we order some for you? (yes or no)"
+  garlic = gets.chomp
+    if garlic == "yes"
+      puts "One order for #{name}"
+      valid_input = true
+    elsif garlic == "no"
+      puts "No garlic bread for #{name}"
+      valid_input = true
+    else
+      puts "I'm sorry yes or no"
+    end
+  end
+    
+    
+valid_input = false
+  until valid_input
+  puts "Would you like to enroll in the company's health insurance?(yes or no)"
+  insurance = gets.chomp
+    if insurance == "yes"
+      puts "We have great medical coverage, #{name}"
+      valid_input = true
+    elsif insurance == "no"
+      puts "#{name}, it's cool I get it."
+      valid_input = true
+    else
+      puts "I'm sorry yes or no"
+    end
+  end
 
-puts "Would you like to enroll in the company's health insurance?"
-insurance = gets.chomp
+
 
 
 
