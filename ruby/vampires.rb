8bc1c6382a6e5f47c24a4ade15e21f165d38e9bb
jtmunoz <jordantmunoz@gmail.com>
2016-08-01 14:22:08 -0700

    adds question insurance

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -8,11 +8,15 @@ puts "What year were you born?"
 year = gets.chomp
 
 puts "Our company cafeteria serves garlic bread. Should we order some for you?"
-answer = gets.chomp
+garlic = gets.chomp
+
+puts "Would you like to enroll in the company's health insurance?"
+insurance = gets.chomp
 
 
 
 p name
 p age
 p year
-p answer
+p garlic
+p insurance
