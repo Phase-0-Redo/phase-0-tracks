46cb4a8154c741f6841c7a771192b01047d3d040
jtmunoz <jordantmunoz@gmail.com>
2016-08-10 16:59:24 -0700

    adds initial commit, adds applicant hash

---
+++ ruby/hashes.rb
@@ -0,0 +1,13 @@
+applicant = {
+  name: nil, 
+  address: nil, 
+  email: nil, 
+  phone: nil, 
+  fave_blue: nil, 
+  likes_paisley: nil, 
+  likes_chevrons: nil, 
+  likes_photorealistic_woods: nil, 
+  likes_abstract_woods: nil, 
+  ombre: nil
+}
+
