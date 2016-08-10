dcb426adedce949f2de4e135915063a448b64d3d
jtmunoz <jordantmunoz@gmail.com>
2016-08-09 19:37:00 -0700

    adds our array and modifies the array

---
+++ ruby/arrays_drill.rb
@@ -0,0 +1,19 @@
+our_array = []
+
+p our_array
+
+our_array << "box" << "shoes" << "keyboard" << "map" << "picture"
+
+p our_array
+
+our_array.delete_at(2)
+
+p our_array
+
+our_array.insert(2, "pencil")
+
+p our_array
+
+our_array.drop(2)
+
+p our_array
