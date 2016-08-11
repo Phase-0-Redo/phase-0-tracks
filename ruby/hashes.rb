27700e4394d25a92b310513687cd6a7f3f04b5a7
jtmunoz <jordantmunoz@gmail.com>
2016-08-11 13:37:18 -0700

    adds until loop for updates

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -52,10 +52,31 @@ applicant[:likes_photorealistic_woods] = gets.chomp
 puts "Do you like abstract woods"
 applicant[:likes_abstract_woods] = gets.chomp
 
-puts "Would you like to update anything?"
-answer = gets.chomp
 
+valid_input = false
 
+until valid_input
+  puts "Would you like to update anything?"
+  answer = gets.chomp
 
+  if answer == "no"
+    puts "Thanks have a nice day!"
+    valid_input = true
+  elsif answer == "yes"
+    puts "What would you like to change?"
+    puts 
+    answer_to_key = gets.chomp
+    puts "Ok, lets change #{answer_to_key} from #{applicant[answer_to_key.to_sym]} to"
+    new_answer = gets.chomp
+    applicant[answer_to_key.to_sym] = new_answer
+    puts "OK CHANGED"
+    puts "Type done if finished updating"
+  elsif answer == "done"
+    puts "OK"
+    valid_input = true
+  else
+    puts "Im sorry yes or no."
+  end
 
+end
 p applicant
