99c9f098d656df7bb0ed27e7da85dcee2659d4c9
jtmunoz <jordantmunoz@gmail.com>
2016-08-12 13:32:23 -0700

    adds keys college and mission to create, also adds key partners for each key

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -4,4 +4,12 @@ to represent a real-world construct.
 something that will use a mix of hashes and arrays.
 =end
 
-starbucks = 
+starbucks = {
+  college_and_plaza: = {
+    partners_working: = ["Chelsea", "Juanita", "Jordan", "Annamarie", "Joel", "Amber", "Jessica", "Ehrin"],
+    
+  },
+  mission_and_el_camino: = {
+    partners_working: = ["Kate", "Renee", "Michelle", "John", "Priscilla", "Sam", "Maille"]
+  }
+}
