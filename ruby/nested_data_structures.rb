368179b8a14b042aaf5e5fab722644e71b36506b
jtmunoz <jordantmunoz@gmail.com>
2016-08-12 14:22:10 -0700

    adds pretty print, adds drive thru and lobby queue

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -3,9 +3,10 @@ design and build a nested data structure
 to represent a real-world construct.
 something that will use a mix of hashes and arrays.
 =end
-
+require 'pp'
 starbucks = {
   college_and_plaza: {
+
     partners_working: ["Chelsea", "Juanita", "Jordan", "Annamarie", "Joel", "Amber", "Jessica", "Ehrin"],
     currently_brewing: {
       medium: "Pike", 
@@ -19,16 +20,24 @@ starbucks = {
         order: {
           drink: "Iced Caramel Macchiatto",
           food: "Chocolate croissant"
-        }
+        },
       },
       car_2: {
         order: {
           drink: "Venti Pike",
           food: nil
-        }
-      }
-    }
+        },
+      },
+    },
     
+    lobby_queue: {
+      customer_1: {
+        order: {
+          drink: "Short Verona",
+          food: "Sausage Sandwich"
+        },
+      },
+    },
 
   },
 
@@ -39,8 +48,42 @@ starbucks = {
       blonde: "Willow", 
       bold: "Sumatra", 
       decaf: "Decaf Pike"
-    }
-  }
+    },
+
+    drive_thru_queue:{
+
+      car_1: {
+        order: {
+          drink: "Grande Iced Coffee",
+          food: "Chocolate croissant"
+        },
+      },
+
+      car_2: {
+        order: {
+          drink: ["Venti Pike", "Tall Cold Brew"],
+          food: ["Pumpkin Bread", "Blueberry Muffin"]
+        }
+      }
+    },
+    
+    lobby_queue: {
+      customer_1: {
+        order: {
+          drink: "Grande Americano",
+          food: ["Coffee Cake", "Protein Bistro Box"]
+        },
+      },
+    },
+  },
 }
 
-p starbucks[:mission_and_el_camino][:currently_brewing][:bold]
+
+#p starbucks[:mission_and_el_camino][:currently_brewing][:bold]
+
+
+
+pp starbucks
+
+p starbucks[:college_and_plaza][:drive_thru_queue][:car_2][:order]
+
