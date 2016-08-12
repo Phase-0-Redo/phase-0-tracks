b03a0fb99d1ffa3750d87dcf71c5eb2244767cb0
jtmunoz <jordantmunoz@gmail.com>
2016-08-12 13:42:21 -0700

    adds hash currently brewing as key to both college and mission

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -5,11 +5,25 @@ something that will use a mix of hashes and arrays.
 =end
 
 starbucks = {
-  college_and_plaza: = {
-    partners_working: = ["Chelsea", "Juanita", "Jordan", "Annamarie", "Joel", "Amber", "Jessica", "Ehrin"],
-    
+  college_and_plaza: {
+    partners_working: ["Chelsea", "Juanita", "Jordan", "Annamarie", "Joel", "Amber", "Jessica", "Ehrin"],
+    currently_brewing: {
+      medium: "Pike", 
+      blonde: "Veranda", 
+      bold: "Verona", 
+      decaf: "Decaf Sumatra"
+    }
   },
-  mission_and_el_camino: = {
-    partners_working: = ["Kate", "Renee", "Michelle", "John", "Priscilla", "Sam", "Maille"]
+
+  mission_and_el_camino: {
+    partners_working: ["Kate", "Renee", "Michelle", "John", "Priscilla", "Sam", "Maille"],
+    currently_brewing: {
+      medium: "Breakfast Blend", 
+      blonde: "Willow", 
+      bold: "Sumatra", 
+      decaf: "Decaf Pike"
+    }
   }
 }
+
+p starbucks[:mission_and_el_camino][:currently_brewing][:bold]
