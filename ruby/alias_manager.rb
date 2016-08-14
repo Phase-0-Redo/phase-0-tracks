9886dd36eeb23149e93d10d1d70674ab1ddae97f
jtmunoz <jordantmunoz@gmail.com>
2016-08-14 15:58:39 -0700

    adds loop to update information

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -10,7 +10,7 @@ to the next vowel in 'aeiou',
   So 'a' would become 'e', 'u' would become 'a', and 'd' 
   would become 'f'
 =end
-
+require 'pp'
 # Swap first and last name.
 def name_swapper(first, last)
   last +" "+ first
@@ -22,10 +22,7 @@ all vowels to the next vowel
 all consonants to the next consonant
 =end
 def name_changer(name)
-      # finds length of a string
-      # name_length = name.length
-      # sets counter to 0 
-      # counter = 0
+      
   #consonants is string of alphabet without vowels
   consonants = "bcdfghjklmnpqrstvwxyz"
   #vowels is a string of vowels
@@ -40,38 +37,102 @@ def name_changer(name)
   elsif letter is a vowel changes it to the next vowel
   join the letters of the array to form 
 =end
-    name_arr.map! do |letter|
-      #letter.next
-      if vowels.include?(letter)
-         vowels[vowels.index(letter) + 1]
-      elsif consonants.include?(letter) 
-         consonants[consonants.index(letter) + 1]
-      elsif letter == "z"
-        letter = "b"
-      elsif letter == "u"
-        letter = "a"
-      end
+  name_arr.map! do |letter|
+    #letter.next
+    #if letter is included within vowels
+    if vowels.include?(letter)
+      # then use the index of the letter within vowels add one for the next vowel
+      vowels[vowels.index(letter) + 1]
+    #else if letter is included within consonants
+    elsif consonants.include?(letter) 
+      # then use the index of the letter within consonants add one for the next 
+      consonants[consonants.index(letter) + 1]
     end
+  end
+  #joins the subletters together to reform the string
   new_name = name_arr.join
   new_name
 end
 
+def storing_info (key, value)
+  info = {}
+  info[key] = value
+  info
+end
 
 puts "Whats your name?"
 first_name = gets.chomp
 new_first = name_changer("#{first_name}")
 puts "Your civilian name #{first_name} is now #{new_first}."
 puts
-
 puts "Whats your last name?"
 last_name = gets.chomp
 new_last = name_changer("#{last_name}")
 puts "Your civilian last name #{last_name} is now #{new_last}."
 puts
-
 old_full = first_name + " " + last_name
 new_full = name_swapper("#{new_first}", "#{new_last}")
 
-p old_full
-p new_full
+  
+stored_info = {
+  real_first: "#{first_name}",
+  real_last: "#{last_name}",
+  spy_first: "#{new_first}",
+  spy_last: "#{new_last}"
+}
+
+pp stored_info
+
+puts
+
+valid_input = false
+until valid_input 
+puts
+puts "Did you like your name? (yes or no or done)"
+  answer = gets.chomp.downcase
+
+  if answer == "no"
+    puts
+    puts "What would you like your new name first name to be?"
+      new_first = gets.chomp
+      stored_info[:real_first] = "#{new_first}"
+      stored_info[:spy_first] = "#{name_changer("#{new_first}")}"
+    puts
+    puts "What you like your new last name to be?"
+      new_last = gets.chomp
+      stored_info[:real_last] = "#{new_last}"
+      stored_info[:spy_last] = "#{name_changer("#{new_last}")}"
+    
+    puts
+    pp stored_info
+
+  elsif answer == "yes"
+    puts
+    puts "Thanks it took a lot of work."
+    valid_input = true
+
+  elsif answer == "done"
+    puts
+    puts "Have a nice day submitting info."
+    valid_input = true
+  else
+    puts
+    puts "Sorry what was that."
+  end
+end
+
+puts
+pp stored_info
+puts
+
+puts "Error"
+puts "Error..."
+puts "..."
+puts "--Transmission Intercepted--"
+puts "This is the most evil organization ever!"
+puts "We know have info on  Agent #{stored_info[:spy_last]} ."
+
+stored_info.each do |id, name|
+  puts "In actuality #{id} name is #{name}."
+end
 
