f967e30434e0b694884f7d40501d8fe86bfbd62e
jtmunoz <jordantmunoz@gmail.com>
2016-08-14 13:40:58 -0700

    modifies name changer with iteration over name_arr changing letters, adds user input

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -11,7 +11,7 @@ to the next vowel in 'aeiou',
   would become 'f'
 =end
 
-#S wap first and last name.
+# Swap first and last name.
 def name_swapper(first, last)
   p last +" "+ first
 end
@@ -21,19 +21,42 @@ changes all the letters,
 all vowels to the next vowel
 all consonants to the next consonant
 =end
-
 def name_changer(name)
-  name_length = name.length
-  counter = 0
-  letters = "bcdfghjklmnpqrstvwxyz"
+      # finds length of a string
+      # name_length = name.length
+      # sets counter to 0 
+      # counter = 0
+  #consonants is string of alphabet without vowels
+  consonants = "bcdfghjklmnpqrstvwxyz"
+  #vowels is a string of vowels
   vowels = "aeiou"
-  new_name = ""
-  name_arr = name.split("")
-  name_arr.map! do |letter| 
-    letter.next
-  end 
-  name_arr.replace("aa")
-  p name_arr
+  #new name is blank string
+  #new_name = ""
+  #split the name into an array of substrings
+  name_arr = name.chars
+=begin 
+  runs iteration for each letter and changes
+  if letter is a consonant changes it to the next consonant
+  elsif letter is a vowel changes it to the next vowel
+  join the letters of the array to form 
+=end
+    name_arr.map! do |letter|
+      #letter.next
+      if vowels.include?(letter)
+         vowels[vowels.index(letter) + 1]
+      elsif consonants.include?(letter) 
+         consonants[consonants.index(letter) + 1]
+      
+      end
+    end
+  new_name = name_arr.join
+  p new_name
 end
 
-p name_changer("buzz")
+
+puts "Whats your name?"
+name = gets.chomp
+
+
+
+name_changer("#{name}")
