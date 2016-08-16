034395e6a4923320d133d0d11cb6b3d25b528e3c
jtmunoz <jordantmunoz@gmail.com>
2016-08-16 10:06:46 -0700

    adds ranking method

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -6,12 +6,14 @@ class Santa
     @location = "Home"
     @gender = gender
     @ethnicity = ethnicity
+    @age = 0
+    @reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
   end
 
+  #sets value of @name
   def get_name
     puts "Whats your name?"
-    name = gets.chomp
-    @name = name
+    @name = gets.chomp
   end
 
   def speak
@@ -23,9 +25,21 @@ class Santa
   end
 
 
+  # changes array reindeer_ranking from most preferred to least into a hash
+  def ranking
+    reindeer_hash = {}
+    puts "Please rank reindeer with numerical value from most favorite to least."
+    @reindeer_ranking.each do |reindeer|
+      p reindeer
+      reindeer_hash[reindeer] = gets.to_i
+    end
+    reindeer_ranking = reindeer_hash
+    p reindeer_ranking
+  end
 end
 
 jordan = Santa.new("male", "wookie")
 jordan.get_name
 jordan.speak
 jordan.eat_milk_and_cookies("brookie")
+jordan.ranking
