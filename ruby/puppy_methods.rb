fbcc7c657487f32701150e067148d2ac7cf62026
jtmunoz <jordantmunoz@gmail.com>
2016-08-16 11:14:02 -0700

    adds Puppy class, adds class methods fetch, speak, rollover, dog_years and favorite food

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -5,5 +5,27 @@ class Puppy
     toy
   end
 
+  def speak(integer)
+    puts "Woof! " * integer
+  end
+
+  def rollover
+    puts "*rollover*"
+  end
+
+  def dog_years(human_years)
+    dog_age = human_years * 7
+    p dog_age
+  end
+
+  def favorite_food(flavor)
+    puts "My favorite flavor of food is #{flavor}."
+  end
 end
 
+spot = Puppy.new
+spot.fetch("ball")
+spot.speak(5)
+spot.rollover
+spot.dog_years(3)
+spot.favorite_food("chicken")
