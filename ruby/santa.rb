92a3630f6be7398f1dd97f69a21891b4620e34f1
jtmunoz <jordantmunoz@gmail.com>
2016-08-16 09:42:15 -0700

    adds attributes gender, ethnicity, adds method get_name

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,9 +1,17 @@
 
 class Santa
-  def initialize
+  def initialize(gender, ethnicity)
     p "Initializing Santa instance...."
-    @name = ""
+    @name = nil
     @location = "Home"
+    @gender = gender
+    @ethnicity = ethnicity
+  end
+
+  def get_name
+    puts "Whats your name?"
+    name = gets.chomp
+    @name = name
   end
 
   def speak
@@ -11,12 +19,13 @@ class Santa
   end
 
   def eat_milk_and_cookies(cookie)
-    p "That was a good #{cookie}"
+    p "#{@name} said hat was a good #{cookie}"
   end
 
 
 end
 
-jordan = Santa.new
+jordan = Santa.new("male", "wookie")
+jordan.get_name
 jordan.speak
 jordan.eat_milk_and_cookies("brookie")
