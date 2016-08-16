889ed47d4d5c878a4dca966b1062d2d4876318a4
jtmunoz <jordantmunoz@gmail.com>
2016-08-16 11:36:56 -0700

    adds class kitty and loops for 50 instances of Kitty

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,4 +1,7 @@
 class Puppy
+  def initialize
+    puts "initializing new puppy instance"
+  end
 
   def fetch(toy)
     puts "I brought back the #{toy}!"
@@ -23,9 +26,38 @@ class Puppy
   end
 end
 
+class Kitty
+  def initialize
+    puts "initializing new kitty instance"
+  end
+
+  def purr
+    puts "PURR!!"
+  end
+
+  def cat_years(human_years)
+    cat_years = human_years * 9
+    p cat_years
+  end
+end
+
+
+
 spot = Puppy.new
 spot.fetch("ball")
 spot.speak(5)
 spot.rollover
 spot.dog_years(3)
 spot.favorite_food("chicken")
+
+hello = Kitty.new
+hello.purr
+hello.cat_years(2)
+
+x = 0
+i = 50
+while x < i
+  Kitty.new 
+  puts "new instance #{x}"
+  x += 1
+end
