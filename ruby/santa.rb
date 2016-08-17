19c9282f4b2aa85297c910582514ef54c226686e
jtmunoz <jordantmunoz@gmail.com>
2016-08-16 17:42:31 -0700

    cleans up code

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -31,19 +31,41 @@ class Santa
     puts "Please rank reindeer with \n numerical value 1 to 9\n 1 being most favorite\n 9 being least."
     @reindeer_ranking.each do |reindeer|
       p reindeer
+      #reindeer_hash[reindeer] = (Random.rand(1..9))
       reindeer_hash[reindeer] = gets.to_i
     end
     @reindeer_ranking = reindeer_hash
-    p "#{@name} Your favorite reindeer is #{@reindeer_ranking.key(9)}"
-    ranking_array = @reindeer_ranking.sort_by {|key, value| value }
-    pp ranking_array
-    pp @reindeer_ranking  
+    p "#{@name} Your favorite reindeer is #{@reindeer_ranking.key(1)}"
+    reindeer_arr = @reindeer_ranking.sort_by {|key, value| value }
+    p reindeer_arr
+    #pp @reindeer_ranking  
+  end
+
+  def age
+    puts "#{@name} how old are you?"
+    @age = gets.to_i
   end
 end
 
-jordan = Santa.new("male", "wookie")
-jordan.get_name
-jordan.speak
-jordan.eat_milk_and_cookies("brookie")
-jordan.ranking
+#jordan = Santa.new("male", "wookie")
+#jordan.get_name
+#jordan.speak
+#jordan.eat_milk_and_cookies("brookie")
+#jordan.ranking
+#jordan.age
+
+santas = []
+example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
+example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
+
+santas = []
+santas << Santa.new("agender", "black")
+santas << Santa.new("female", "Latino")
+santas << Santa.new("bigender", "white")
+santas << Santa.new("male", "Japanese")
+santas << Santa.new("female", "prefer not to say")
+santas << Santa.new("gender fluid", "Mystical Creature (unicorn)")
+santas << Santa.new("N/A", "N/A")
+
+p santas[1]
 
