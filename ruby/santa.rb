467204177a217a3ea5379e06eb4e8c3c86c9befd
jtmunoz <jordantmunoz@gmail.com>
2016-08-17 21:23:12 -0700

    adds completes reindeer ranking method

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -2,12 +2,15 @@ require 'pp'
 class Santa
   def initialize(gender, ethnicity)
     p "Initializing Santa instance...."
-    @location = "Home"
     @gender = gender
     @ethnicity = ethnicity
+    @reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
   end
+  @position = 0
   @age = 0
-  @age = 0
+  @name = ''
+  @location = "Home"
+  
   #sets value of @name
   def get_name
     puts "Whats your name?"
@@ -22,22 +25,29 @@ class Santa
     p "#{@name} said hat was a good #{cookie}"
   end
 
-  @reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+  #@reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+ 
   # changes array reindeer_ranking from most preferred to least into a hash
   def ranking
     reindeer_hash = {}
     puts "Please rank reindeer with \n numerical value 1 to 9\n 1 being most favorite\n 9 being least."
     @reindeer_ranking.each do |reindeer|
       p reindeer
-      #reindeer_hash[reindeer] = (Random.rand(1..9))
       reindeer_hash[reindeer] = gets.to_i
     end
+    reindeer_hash
     @reindeer_ranking = reindeer_hash
     p "#{@name} Your favorite reindeer is #{@reindeer_ranking.key(1)}"
-    reindeer_arr = @reindeer_ranking.sort_by {|key, value| value }
-    p reindeer_arr
-    #pp @reindeer_ranking  
+    reindeer_hash.sort_by {|key, value| value }
+    @reindeer_ranking = reindeer_hash
+    pp @reindeer_ranking  
+    reindeer_arr = []
+    @reindeer_ranking.each do |key, value|
+      reindeer_arr.insert(value - 1, key)
+    end
+    @reindeer_ranking = reindeer_arr.compact!
   end
+
 
   def age
     puts "#{@name} how old are you?"
@@ -45,28 +55,51 @@ class Santa
   end
 end
 
-#jordan = Santa.new("male", "wookie")
+jordan = Santa.new("male", "wookie")
 #jordan.get_name
 #jordan.speak
 #jordan.eat_milk_and_cookies("brookie")
-#jordan.ranking
+jordan.ranking
 #jordan.age
 
-santas = []
-example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
-example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
+p jordan
 
 #santas = []
-#santas << Santa.new("agender", "black")
-#santas << Santa.new("female", "Latino")
-#santas << Santa.new("bigender", "white")
-#santas << Santa.new("male", "Japanese")
-#santas << Santa.new("female", "prefer not to say")
-#santas << Santa.new("gender fluid", "Mystical Creature (unicorn)")
-#santas << Santa.new("N/A", "N/A")
+#example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
+#example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
+#
+##santas = []
+##santas << Santa.new("agender", "black")
+##santas << Santa.new("female", "Latino")
+##santas << Santa.new("bigender", "white")
+##santas << Santa.new("male", "Japanese")
+##santas << Santa.new("female", "prefer not to say")
+##santas << Santa.new("gender fluid", "Mystical Creature (unicorn)")
+##santas << Santa.new("N/A", "N/A")
+#def multiply(x,y)
+#  x * y
+#end
+#
+#
+#
+#counter = 0
+#random_gender_integer = Random.rand(1..example_genders.length) 
+#random_ethnicities_integer = Random.rand(1..example_ethnicities.length)
+#
+#random_integer = multiply(random_gender_integer, random_ethnicities_integer)
+#
+#until counter == random_integer
+#  santas << Santa.new(example_genders[Random.rand(example_genders.length + 1)], example_ethnicities[Random.rand(example_ethnicities.length + 1)])
+#  puts "There are now #{santas.length}"
+#  counter += 1
+#end 
+#
+#pp santas
+#p santas.length
+#puts "The counter length is #{counter}" 
+#puts "Result of random integer based upon example_genders length is #{random_gender_integer}."
+#puts "Result of random integer based upon example example_ethnicities length is #{random_ethnicities_integer}."
+#p random_integer
+#pp @reindeer_ranking
 
-example_genders.length.times do |i|
-  santas << Santa.new(example_genders[Random.rand(example_genders.length + 1)], example_ethnicities[Random.rand(example_ethnicities.length + 1)])
-end
 
-pp santas
