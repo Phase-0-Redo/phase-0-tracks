06eac7ad966d877be2a0e5061b815ed2f7c66f5d
jtmunoz <jordantmunoz@gmail.com>
2016-08-19 15:18:49 -0700

    refactors code for selecting number of instances

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -19,7 +19,6 @@ class Santa
     @name = gets.chomp
   end
 
-  
   def age
     puts "#{@name} how old are you?"
     @age = gets.to_i
@@ -40,18 +39,15 @@ class Santa
   # changes array reindeer_ranking from most preferred to least into a hash
   def ranking
     reindeer_hash = {}
+    reindeer_arr = []
     puts "Please rank reindeer with \n numerical value 1 to 9\n 1 being most favorite\n 9 being least."
       @reindeer_ranking.each do |reindeer|
         p reindeer
         reindeer_hash[reindeer] = gets.to_i
       end
-    reindeer_hash
     @reindeer_ranking = reindeer_hash
     p "#{@name} Your favorite reindeer is #{@reindeer_ranking.key(1)}"
     reindeer_hash.sort_by {|key, value| value }
-    @reindeer_ranking = reindeer_hash
-    pp @reindeer_ranking  
-    reindeer_arr = []
       @reindeer_ranking.each do |key, value|
         reindeer_arr.insert(value - 1, key)
       end
@@ -59,21 +55,15 @@ class Santa
   end
 
   def get_mad_at
-    #puts "Which reindeer is in trouble?"
-    #reindeer = gets.chomp.capitalize!
-
-    #@reindeer_ranking.include?(reindeer) = false
     valid_input = false
     until valid_input == true  
       puts "Which reindeer is in trouble?"
       reindeer = gets.chomp.capitalize!
         if @reindeer_ranking.include?(reindeer) == false
-          puts "Sorry one of Santa's known reindeers."
-          
+          puts "Sorry one of Santa's known reindeers." 
         elsif @reindeer_ranking.include?(reindeer) == true
           @reindeer_ranking.delete(reindeer)
           @reindeer_ranking << reindeer
-        #@reindeer_ranking.include?(reindeer) = false
           valid_input = true
         end
     end
@@ -81,11 +71,15 @@ class Santa
   end
 end
 
+def multiply(x,y)
+  x * y
+end
+
 jordan = Santa.new("male", "wookie")
 #jordan.get_name
 #jordan.speak
 #jordan.eat_milk_and_cookies("brookie")
-#jordan.ranking
+jordan.ranking
 jordan.age
 jordan.name
 jordan.get_mad_at
@@ -93,41 +87,34 @@ jordan.gender = "anti-gender"
 jordan.ethnicity = "mohican"
 jordan.celebrate_birthday
 jordan.age = 79
+jordan.gender = "stormtrooper"
 pp jordan
 
 
 
-#santas = []
-#example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
-#example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
-#
-##santas = []
-##santas << Santa.new("agender", "black")
-##santas << Santa.new("female", "Latino")
-##santas << Santa.new("bigender", "white")
-##santas << Santa.new("male", "Japanese")
-##santas << Santa.new("female", "prefer not to say")
-##santas << Santa.new("gender fluid", "Mystical Creature (unicorn)")
-##santas << Santa.new("N/A", "N/A")
-#def multiply(x,y)
-#  x * y
-#end
-#
-#
-#
-#counter = 0
-#random_gender_integer = Random.rand(1..example_genders.length) 
-#random_ethnicities_integer = Random.rand(1..example_ethnicities.length)
-#
-#random_integer = multiply(random_gender_integer, random_ethnicities_integer)
-#
+santas = []
+example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
+example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
+
+counter = 0
+random_gender_integer = Random.rand(1..example_genders.length) 
+random_ethnicities_integer = Random.rand(1..example_ethnicities.length)
+random_integer = multiply(random_gender_integer, random_ethnicities_integer)
+
 #until counter == random_integer
 #  santas << Santa.new(example_genders[Random.rand(example_genders.length + 1)], example_ethnicities[Random.rand(example_ethnicities.length + 1)])
 #  puts "There are now #{santas.length}"
 #  counter += 1
-#end 
+#end
+
+until counter == random_integer
+  santas << Santa.new(example_genders.sample, example_ethnicities.sample)
+  puts "There are now #{santas.length}"
+  counter += 1
+end 
+
 #
-#pp santas
+pp santas
 #p santas.length
 #puts "The counter length is #{counter}" 
 #puts "Result of random integer based upon example_genders length is #{random_gender_integer}."
