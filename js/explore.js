a9cdda9d4666afc58b90bd4bf308c738e3289700
jtmunoz <jordantmunoz@gmail.com>
2016-08-23 11:24:52 -0700

    calls function and stores into variable, adds driver code to print variable for if condition

--- js/explore.js
+++ js/explore.js
@@ -9,5 +9,18 @@ function reverse(string){
   console.log(str_arr);
 }
 
-reverse("reverse");
-reverse("forward");
+
+//stores function reverse into variable reverse_string
+//var reverse_string = console.log(reverse("reverse"));
+
+//reverse("reverse");
+var y = reverse("reverse");
+y
+//console.log(reverse_string);
+//console.log(reverse_string);
+
+if (1 == 1){
+  y ;
+} else {
+  console.log("YUP");
+}
