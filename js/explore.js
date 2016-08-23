992e11719823c06327b3d4fe46805f4c0fdfe922
jtmunoz <jordantmunoz@gmail.com>
2016-08-23 10:56:02 -0700

    adds psuedocode and split reverse and join to reverse function

--- js/explore.js
+++ js/explore.js
@@ -1,11 +1,13 @@
 // a function takes a string as a parameter 
 //reverses the string
 function reverse(string){
-  //var str_length = string.length;
-  var str_arr = string.split('');
-  str_arr.reverse();
-  str_arr.join();
+  //splits the string into an array
+  //reverses the array
+  //joins the array
+  var str_arr = string.split('').reverse().join('');
+  //logs the array to the console
   console.log(str_arr);
 }
 
-console.log(reverse("reverse"));
+reverse("reverse");
+reverse("forward");
