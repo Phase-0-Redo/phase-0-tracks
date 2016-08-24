8be17ea4974d70edc8c79683189303eb90f8c4f3
jtmunoz <jordantmunoz@gmail.com>
2016-08-24 14:33:52 -0700

    initial commit

---
+++ js/algos.js
@@ -0,0 +1,16 @@
+//function takes an array of words
+// returns the longest phrase
+
+//input is an array
+//sorts through the array
+//organizes based on length
+//iterates over array with length 
+//organizes based upon length
+//logs the longest
+
+var words = ["long phrase","longest phrase","longer phrase"]
+function sort(array){
+array.length
+}
+
+console.log(sort(words));
