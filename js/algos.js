71c91d111a106c6c668af1a1b681a4d5f3c8b453
jtmunoz <jordantmunoz@gmail.com>
2016-08-25 13:12:16 -0700

    refactors code, updates pseudocode

--- js/algos.js
+++ js/algos.js
@@ -1,16 +1,41 @@
 //function takes an array of words
 // returns the longest phrase
 
-//input is an array
+
 //sorts through the array
 //organizes based on length
-//iterates over array with length 
 //organizes based upon length
 //logs the longest
 
-var words = ["long phrase","longest phrase","longer phrase"]
-function sort(array){
-array.length
+
+
+//function accepts an array as a paramter 
+//prints the longest item in the array
+function printer(array){
+
+  //stores array as variable
+  var arr = array
+
+  //sorts over the array with the function 
+  //comparing length of items
+  arr.sort(function(a, b){
+    return b.length - a.length;
+  });
+
+  // stores the last item of the array into variable
+  var longest = arr.shift()
+
+  //returns the the variable longest
+  return longest;
 }
 
-console.log(sort(words));
+
+//adds code for testing
+var words = ["long phrase","longest phrase","longer phrase"];
+words.unshift("even longer phrase");
+words.push("phrase");
+//console.log(words);
+
+//driver code for printer function
+console.log(printer(words));
+
