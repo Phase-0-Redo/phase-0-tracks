8227d18d482a44114b69e5574f723edbb345cd91
jtmunoz <jordantmunoz@gmail.com>
2016-08-26 16:18:11 -0700

    initial push, adds colors and names array

---
+++ js/data_structures.js
@@ -0,0 +1,9 @@
+var colors = ["blue", "green", "red", "magenta"];
+var names = ["Ed", "Jordan", "Jill", "Ellie"];
+
+colors.push("cyan");
+names.shift("Jeffrey");
+
+console.log(colors);
+console.log(names);
+
