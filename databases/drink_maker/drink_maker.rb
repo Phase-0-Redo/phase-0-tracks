7e9d257dbb89e55860b31ea25c50a06e3edb633c
jtmunoz <jordantmunoz@gmail.com>
2016-09-04 13:47:43 -0700

    adds create drink method, runs iterations

--- databases/drink_maker/drink_maker.rb
+++ databases/drink_maker/drink_maker.rb
@@ -4,12 +4,12 @@ require 'faker'
 
 
 #create SQLITE3 database
-db = SQLite3::Databas.new("drinks.db")
-db.result_as_hash = true
+db = SQLite3::Database.new("drinks.db")
+db.results_as_hash = true
 
 #string delimiters
 create_table_cmd = <<-SQL
-  CREATE TABLE IF NOT EXISTS kittens(
+  CREATE TABLE IF NOT EXISTS drinks(
     id INTEGER PRIMARY KEY,
     name VARCHAR(255),
     shots INT
@@ -21,4 +21,19 @@ db.execute(create_table_cmd)
 
 #test drink
 db.execute("INSERT INTO drinks (name, shots) VALUES ('Bob', 10)")
+
+
+def create_drink(db, name, age)
+  db.execute("INSERT INTO drinks (name, shots) VALUES (?, ?)", [name, age])
+end
+
+10.times do
+  create_drink(db, Faker::Name.name, Faker::Number.number(1))
+end
+
+drinks = db.execute("SELECT * FROM drinks")
+drinks.each do |drink|
+  puts "#{drink['name']} has #{drink['shots']} shots"
+end
+
 
